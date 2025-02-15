b390ac307b0b08c25f2c7c4f7de8dc8024cdc3a2
atumah <atumahgerald48@gmail.com>
2025-02-15 20:34:21 +0100

    add databased and table creation

---
+++ DTT assessment  - Backend_2023 copy/web_backend_test_catering_api/web_backend_test_catering_api/web_backend_test_catering_api/config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'db' => [
+        'host' => 'localhost',
+        'database' => 'facility_management',
+        'username' => 'root',
+        'password' => 'Ogheneruemu',
+    ],
+];
